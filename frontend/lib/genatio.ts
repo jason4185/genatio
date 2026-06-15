d14addd99337376d1a3ab9e54b02eb97bd4d5f64
jason4185 <akusobifavour7@gmail.com>
2026-06-15 06:28:54 +0100

    Update dispute contract address

--- frontend/lib/genatio.ts
+++ frontend/lib/genatio.ts
@@ -2,6 +2,6 @@ import { createClient } from "genlayer-js";
 import { testnetBradbury as glTestnetBradbury } from "genlayer-js/chains";
 
 export const GENATIO_CONTRACT = "0x23a0342Edc685fcCb50b3e3C2a86318c93d79942";
-export const DISPUTE_CONTRACT = "0x85B70dafE892Aefcd0e75de544dB54Cf113cD88A";
+export const DISPUTE_CONTRACT = "0x569c9E3C9Ac23444D334710e507E1035fe09283F";
 
 export const genLayerClient = createClient({ chain: glTestnetBradbury });
